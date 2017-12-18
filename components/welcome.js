d0e58ee80a9c2606d6bb57fe1c006bbaf9ee236e
Arthur Corenzan <arthur@corenzan.com>
2017-12-18 20:02:34 -0200

    Replace META with COMMAND in instructions #8

--- components/welcome.js
+++ components/welcome.js
@@ -5,7 +5,7 @@ export default class Welcome extends Component {
 		return (
 			<section class="welcome">
 				<h1>Paste your <strong>Local</strong> here...</h1>
-				<p>Simply press <code>CTRL+V</code> (or <code>META+V</code> in macOS) and it'll check character's affiliations, threat level, kills, and losses.</p>
+				<p>Simply press <code>CTRL+V</code> (or <code>COMMAND+V</code> in macOS) and it'll fetch character's affiliations, threat level, kills, and losses.</p>
 			</section>
 		);
 	}
