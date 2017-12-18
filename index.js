96440136ccaac70761f220af67f845c880f41706
Arthur Corenzan <arthur@corenzan.com>
2017-12-18 20:34:48 -0200

    Truncate pastes longer than 500 lines #5

--- index.js
+++ index.js
@@ -12,24 +12,31 @@ ReactGA.pageview('/');
 
 export default class App extends Component {
   state = {
-    characters: [],
+    notification: '',
+    paste: [],
   };
 
   onPaste = (e) => {
     const clipboard = e.clipboardData || window.clipboardData;
     const text = clipboard.getData('Text');
     if (!text) return;
-    const characters = text.split("\n").map((name) => {
+    const paste = text.split("\n").map((name) => {
       return { name, dangerRatio: 0 };
     });
-    this.setState({ characters });
+    let notification = '';
+    if (paste.length > 100) {
+      paste.splice(100);
+      notification = 'Natural phenomena disrupts this paste (only 100 names at a time).';
+    }
+    this.setState({ paste, notification });
   };
 
-  render({}, { characters }) {
+  render({}, { notification, paste }) {
     return (
       <main onPaste={this.onPaste}>
-        {characters.length ? (<Table characters={characters} />) : (<Welcome />)}
-        <footer class="foot">
+        {notification ? (<p class="notification"><span class="notification__icon">!</span> {notification}</p>) : ''}
+        {paste.length ? (<Table characters={paste} />) : (<Welcome />)}
+        <footer class="footer">
           <p class="localthreat"> <a href="https://localthreat.xyz">localthreat.xyz</a> &copy; 2017 Arthur Corenzan &middot; <a href="https://github.com/haggen/localthreat" target="blank" rel="noopener noreferrer">GitHub</a> &middot; Data provided by <a href="https://esi.tech.ccp.is/latest/" target="blank" rel="noopener noreferrer">ESI</a> and <a href="https://zkillboard.com/" target="blank" rel="noopener noreferrer">zKillboard</a> &middot; Tips go to <a href="https://zkillboard.com/character/95036967/" target="blank" rel="noopener noreferrer">Jason Chorant</a></p>
           <p class="ccp">EVE Online and the EVE logo are the registered trademarks of CCP hf. All rights are reserved worldwide. All other trademarks are the property of their respective owners. EVE Online, the EVE logo, EVE and all associated logos and designs are the intellectual property of CCP hf. All artwork, screenshots, characters, vehicles, storylines, world facts or other recognizable features of the intellectual property relating to these trademarks are likewise the intellectual property of CCP hf. CCP hf. has granted permission to localthreat.xyz to use EVE Online and all associated logos and designs for promotional and information purposes on its website but does not endorse, and is not in any way affiliated with, localthreat.xyz. CCP is in no way responsible for the content on or functioning of this website, nor can it be liable for any damage arising from the use of this website.</p>
         </footer>
