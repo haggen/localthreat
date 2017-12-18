cbcd6197cd86accefc6a7b4c60acf3da1afad735
Arthur <arthur@corenzan.com>
2017-12-18 08:57:16 -0200

    Add footer with links and legal notice

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ export default class App extends Component {
 		return (
 			<main onPaste={this.onPaste}>
 				{characters.length ? (<Table characters={characters} />) : (<Welcome />)}
+				<footer class="foot">
+					<p class="localthreat">
+						<a href="/">localthre.at</a> &copy; 2017 Arthur Corenzan &middot; <a href="https://github.com/haggen/localthre.at">GitHub</a> &middot; Tips go to <a href="https://zkillboard.com/character/95036967/">Jason Chorant</a>
+					</p>
+					<p class="ccp">EVE Online and the EVE logo are the registered trademarks of CCP hf. All rights are reserved worldwide. All other trademarks are the property of their respective owners. EVE Online, the EVE logo, EVE and all associated logos and designs are the intellectual property of CCP hf. All artwork, screenshots, characters, vehicles, storylines, world facts or other recognizable features of the intellectual property relating to these trademarks are likewise the intellectual property of CCP hf. CCP hf. has granted permission to zKillboard.com to use EVE Online and all associated logos and designs for promotional and information purposes on its website but does not endorse, and is not in any way affiliated with, zKillboard.com. CCP is in no way responsible for the content on or functioning of this website, nor can it be liable for any damage arising from the use of this website.</p>
+				</footer>
 			</main>
 		);
 	}
