5c4c0ff563bfba2df8d17a02a98338a1e2fb7646
Arthur <arthur@corenzan.com>
2017-12-18 23:58:10 -0200

    Update instruction text

--- components/welcome.js
+++ components/welcome.js
@@ -5,7 +5,7 @@ export default class Welcome extends Component {
     return (
       <section class="welcome">
         <h1 class="welcome__heading">Paste your <strong>Local</strong> here...</h1>
-        <p class="welcome__instruction">Simply press <code>CTRL+V</code> (or <code>COMMAND+V</code> in macOS) and it'll fetch character's affiliations, threat level, kills, and losses.</p>
+        <p class="welcome__instruction">Simply press <code>CTRL+V</code> (or <code>COMMAND+V</code> in macOS) and it'll fetch character's affiliations, threat level, and other combat stats.</p>
       </section>
     );
   }
