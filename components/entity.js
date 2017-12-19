022e6124b7f1f88b66d0b20f9e60d99f2fddb812
Arthur <arthur@corenzan.com>
2017-12-18 22:58:50 -0200

    Use flex for figure/text vertical alignment

--- components/entity.js
+++ components/entity.js
@@ -4,10 +4,12 @@ export default class Entity extends Component {
   render({ href, image, size, name }, {}) {
     return (
       <a href={href} class="entity" target="blank" rel="noopener noreferrer">
-        <span class="entity__picture">
+        <figure class="entity__picture">
           <img src={image} width={size} height={size} alt={name} />
+        </figure>
+        <span class="entity__name">
+          {name}
         </span>
-        {name}
       </a>
     );
   }
