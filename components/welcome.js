9cf0eba6bdeabd5d557704ebf8c9a0c70385dfec
Arthur <arthur@corenzan.com>
2017-12-30 15:34:01 -0200

    Make Welcome a functional component

--- components/welcome.js
+++ components/welcome.js
@@ -1,12 +1,10 @@
-import { Component } from 'preact';
+const Welcome = _ => {
+  return (
+    <section class="welcome">
+      <h1 class="welcome__heading">Paste your <strong>Local</strong> here...</h1>
+      <p class="welcome__instruction">Press <code>CTRL+V</code> (or <code>COMMAND+V</code> in macOS) to get characters affiliations and PvP stats.</p>
+    </section>
+  );
+};
 
-export default class Welcome extends Component {
-  render() {
-    return (
-      <section class="welcome">
-        <h1 class="welcome__heading">Paste your <strong>Local</strong> here...</h1>
-        <p class="welcome__instruction">Press <code>CTRL+V</code> (or <code>COMMAND+V</code> in macOS) to get characters affiliations and PvP stats.</p>
-      </section>
-    );
-  }
-}
+export default Welcome;
