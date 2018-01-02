77241ca9b23417305dd43bfde63855b798e3cdf5
Arthur <arthur@corenzan.com>
2018-01-02 02:15:31 -0200

    Fix copy directory

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,5 @@
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 export default config => {
-  config.plugins.push(new CopyWebpackPlugin([{ context: `${__dirname}/src/assets`, from: `*.*` }]));
+  config.plugins.push(new CopyWebpackPlugin([{ context: `${__dirname}/assets`, from: `*.*` }]));
 };
