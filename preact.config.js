54720975a302824743a554cf3ce31456843089ac
Arthur Corenzan <arthur@corenzan.com>
2018-01-09 11:16:41 -0200

    Copy _redirects but without including it into sw-precache list

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,7 @@
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 export default config => {
-  config.plugins.push(new CopyWebpackPlugin([{ context: `${__dirname}/assets`, from: `*` }]));
+  config.plugins.push(new CopyWebpackPlugin([
+    { from: 'static', to: '.' }
+  ]));
 };
