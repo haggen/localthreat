fb09baca3934944927f1b017e5330fd2352474ee
Arthur <arthur@corenzan.com>
2019-01-08 10:19:18 -0200

    Make the service worker ignore Netlify config file _redirects

--- preact.config.js
+++ preact.config.js
@@ -1,7 +1,14 @@
-import CopyWebpackPlugin from 'copy-webpack-plugin';
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import preactCliSwPrecachePlugin from "preact-cli-sw-precache";
 
 export default config => {
-  config.plugins.push(new CopyWebpackPlugin([
-    { from: 'static', to: '.' }
-  ]));
+  config.plugins.push(
+    new CopyWebpackPlugin([{ context: ".", from: "_redirects" }])
+  );
+
+  const precacheConfig = {
+    staticFileGlobsIgnorePatterns: [/_redirects/]
+  };
+
+  return preactCliSwPrecachePlugin(config, precacheConfig);
 };
