ccec90d2f09ba1cd527b462a1ada69456e94e5d8
Arthur <arthur@corenzan.com>
2020-09-18 23:32:37 -0300

    Drop sw setup, since we can't ever be offline

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "components/app";
-import * as serviceWorker from "lib/service-worker";
 
 import "resetize";
 import "./global.css";
@@ -12,8 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
