eed10200c4154adee79bc90a902a891adb5f0b0f
Arthur <arthur@corenzan.com>
2020-09-19 14:58:37 -0300

    Fix CORS headers

--- api/web/cors_handler.go
+++ api/web/cors_handler.go
@@ -12,12 +12,15 @@ func CORSHandler() Middleware {
 				w.Header().Set("Access-Control-Allow-Origin", o)
 			}
 			if r.Method == http.MethodOptions {
-				w.Header().Set("Access-Control-Allow-Method", r.Header.Get("Access-Control-Request-Method"))
-				w.Header().Set("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
+				w.Header().Set("Access-Control-Allow-Methods", r.Header.Get("Access-Control-Request-Method"))
+				if headers := r.Header.Get("Access-Control-Request-Headers"); headers != "" {
+					w.Header().Set("Access-Control-Allow-Headers", headers)
+				}
 				w.Header().Set("Access-Control-Max-Age", "86400")
 				w.WriteHeader(http.StatusNoContent)
 				return
 			}
+			w.Header().Set("Vary", "origin")
 			next.ServeHTTP(w, r)
 		})
 	}
