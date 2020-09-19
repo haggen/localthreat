283afbe0126fa8bea876336e98843a449d917945
Arthur <arthur@corenzan.com>
2020-09-18 23:32:22 -0300

    Remove left over file

--- client/src/lib/x.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import { Character } from "types";
-
-type Props = {
-  data: Character[];
-  update: (data: Character) => void;
-};
-
-export const Data = createContext<Props>({ data: [], update(_) {} });
