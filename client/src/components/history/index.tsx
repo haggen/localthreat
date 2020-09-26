0bc3598303e60540345ed8b558a34a3aadd9a0f0
Arthur <arthur@corenzan.com>
2020-09-26 10:54:28 -0300

    History panel wip (#30)

---
+++ client/src/components/history/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Drawer } from "components/drawer";
+
+export const History = () => {
+  return (
+    <Drawer name="history" title="History" side="right">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio atque in
+        blanditiis voluptatum modi tenetur libero est natus, sequi nobis
+        nesciunt officiis eos earum provident quas magni deleniti ratione ipsa!
+      </p>
+    </Drawer>
+  );
+};
