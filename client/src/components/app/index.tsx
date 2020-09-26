22f9d3a970e393ba0b3876699964fd2c3eb85217
Arthur <arthur@corenzan.com>
2020-09-26 10:53:54 -0300

    Include News and Help panels

--- client/src/components/app/index.tsx
+++ client/src/components/app/index.tsx
@@ -4,11 +4,21 @@ import { Welcome } from "components/welcome";
 import { Report } from "components/report";
 import { Topbar } from "components/topbar";
 import { Footer } from "components/footer";
+import { News } from "components/news";
+// import { History } from "components/history";
+import { Help } from "components/help";
+// import { Settings } from "components/settings";
+import { DrawerProvider } from "components/drawer";
+
 import style from "./style.module.css";
 
 export const App = () => {
   return (
-    <>
+    <DrawerProvider>
+      <News />
+      {/* <Settings /> */}
+      <Help />
+      {/* <History /> */}
       <Topbar />
       <main className={style.main}>
         <Switch>
@@ -17,6 +27,6 @@ export const App = () => {
         </Switch>
       </main>
       <Footer />
-    </>
+    </DrawerProvider>
   );
 };
