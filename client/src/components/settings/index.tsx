56d2356dd90b28c2920f63ada2cb6b5048e6200d
Arthur <arthur@corenzan.com>
2020-09-26 10:54:39 -0300

    Settings panel wip (#37)

---
+++ client/src/components/settings/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Drawer } from "components/drawer";
+
+export const Settings = () => {
+  return (
+    <Drawer name="settings" title="Settings" side="left">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio atque in
+        blanditiis voluptatum modi tenetur libero est natus, sequi nobis
+        nesciunt officiis eos earum provident quas magni deleniti ratione ipsa!
+      </p>
+    </Drawer>
+  );
+};
