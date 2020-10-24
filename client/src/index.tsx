017f1773098074cff5e13015e5ad4d03e296bf60
Arthur <arthur@corenzan.com>
2020-10-24 07:47:59 -0300

    Register service-worker

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { register } from "lib/service-worker-registration";
 import { App } from "components/app";
 
 import "resetize";
@@ -11,3 +12,9 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+register();
+
