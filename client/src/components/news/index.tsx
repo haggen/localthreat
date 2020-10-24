21978d33e9a4e43c04b5363c8741af6aab66c96c
Arthur <arthur@corenzan.com>
2020-10-24 07:09:42 -0300

    Bump news dates to coincide with launch

--- client/src/components/news/index.tsx
+++ client/src/components/news/index.tsx
@@ -5,7 +5,7 @@ import { Article } from "components/article";
 export const News = () => {
   return (
     <Drawer name="news" title="News" side="left">
-      <Article title="Known issues" date={new Date(2020, 8, 25)}>
+      <Article title="Known issues" date={new Date(2020, 9, 24)}>
         <p>
           As it usually is we still have a few kinks to iron out that you may or
           may not encounter while using the site.
@@ -24,7 +24,7 @@ export const News = () => {
           </li>
         </ul>
       </Article>
-      <Article title="Launching version 2.0" date={new Date(2020, 8, 25)}>
+      <Article title="Launching version 2.0" date={new Date(2020, 9, 24)}>
         <p>
           The whole thing has been rebuilt from the ground up now with the
           latest technology, plus a few goodies:
