dd7182db6842bdff3e80c271f29767fc7277661a
Arthur <arthur@corenzan.com>
2020-10-24 07:48:08 -0300

    Add web-vitals

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { register } from "lib/service-worker-registration";
+import { report } from "lib/web-vitals";
 import { App } from "components/app";
 
 import "resetize";
@@ -18,3 +19,7 @@ ReactDOM.render(
 // Learn more about service workers: https://cra.link/PWA
 register();
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+report();
