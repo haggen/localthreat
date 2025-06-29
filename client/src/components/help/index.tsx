c9d467c85d85a4b0afaf88f001a395e5239dfe9e
Arthur <arthur@corenzan.com>
2025-06-29 13:17:29 -0300

    Improve help text

--- client/src/components/help/index.tsx
+++ client/src/components/help/index.tsx
@@ -1,30 +1,31 @@
-import React from "react";
-import { Drawer } from "components/drawer";
 import { Article } from "components/article";
+import { Drawer } from "components/drawer";
+import React from "react";
 
 export const Help = () => {
   return (
     <Drawer name="help" title="Help" side="left">
       <Article title="What is localthreat?">
         <p>
-          It's a website to help players of EVE Online quickly identify friend
-          from foe and assess the level of threat their enemy may present.
+          It's a website that helps players of EVE Online quickly identify
+          friend from foe and assess the level of threat their enemies may
+          present.
         </p>
       </Article>
       <Article title="How does it work?">
         <p>
-          With the window in focus it listens to paste events from the user.
-          You'll need to copy a chat's transcript or a channel members list from
-          in-game and press CTRL+V on this page. Once an input is detected it
+          With the window in focus, it listens for paste events from the user.
+          You'll need to copy a chat transcript or a channel member list from
+          in-game and press CTRL+V on this page. Once input is detected, it
           parses the content for character names and queries information from
-          the game servers and other third-party APIs to offer a complete a
-          report of affiliations and PvP statistics of those players.
+          the game servers and other third-party APIs to offer a complete report
+          of affiliations and PvP statistics for those players.
         </p>
       </Article>
       <Article title="What else can it do?">
         <ul>
           <li>
-            A report will have an unique URL that can be shared with your
+            A report will have a unique URL that can be shared with your
             friends.
           </li>
           <li>
@@ -32,10 +33,10 @@ export const Help = () => {
             stats.
           </li>
           <li>
-            Pasting on an existing report will add non duplicate names to it.
+            Pasting into an existing report will add non-duplicate names to it.
           </li>
           <li>
-            A history of visited reports will be stored locally on your browser
+            A history of visited reports will be stored locally in your browser
             and can be accessed using the History panel.
           </li>
           <li>
@@ -45,8 +46,8 @@ export const Help = () => {
       </Article>
       <Article title="Does it cost anything?">
         <p>
-          To you nothing. localthreat is completely free to use, displays no ads
-          and also has its source code released under a fairly permissive
+          Nothing to you. localthreat is completely free to use, displays no
+          ads, and also has its source code released under a fairly permissive
           license on GitHub. Go{" "}
           <a
             href="https://github.com/haggen/localthreat"
@@ -60,7 +61,7 @@ export const Help = () => {
       </Article>
       <Article title="I have other questions.">
         <p>
-          If you'd like we can chat about what's on your mind. You may{" "}
+          If you'd like, we can chat about what's on your mind. You may{" "}
           <a
             href="https://github.com/haggen/localthreat/issues/new"
             target="blank"
@@ -68,8 +69,8 @@ export const Help = () => {
           >
             open an issue on GitHub
           </a>{" "}
-          or send an EVE-mail to <strong>Jason Chorant</strong> and I'll get
-          back to you, eventually.
+          or send an EVE-mail to <strong>Jason Chorant</strong>, and I'll get
+          back to you—eventually.
         </p>
       </Article>
     </Drawer>
