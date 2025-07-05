172859f6e42ea84946d2aefd7627e93b92a579cf
Arthur <arthur@corenzan.com>
2025-07-05 17:25:58 -0300

    Add migrate program to retore old data

---
+++ api/migrate/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/csv"
+	"log"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./storage/database.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file, err := os.Open("./dump.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			log.Fatal(err)
+		}
+
+		id := record[0]
+		createdAt := record[1]
+		source := record[2]
+
+		_, err = db.Exec(`INSERT INTO reports (id, createdAt, source) VALUES (?, ?, ?)`, id, createdAt, source)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
