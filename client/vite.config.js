688d887c4f62236f70d8b2396c3349d6b26f25be
Arthur Corenzan <arthur@corenzan.com>
2026-08-20 10:10:08 -0300

    Drop unecessary plugin

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,9 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { join } from "path";
 import { defineConfig } from "vite";
-import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "~": join(import.meta.dirname, "src"),
